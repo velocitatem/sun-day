4e47bd09b70249788354afd4cc8fb56b8acbb0bf
Daniel Rosel <danalves24@outlook.com>
2020-04-06 10:19:57 +0200

    header

--- src/wetpi.js
+++ src/wetpi.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styled from 'styled-components'
 import $ from 'jquery';
+const Title = styled.h2`
+text-align: center;
+`
+const Find = styled.button`
+height: 0.8cm;
+background-color: light-gray;
+border: none;
+`
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <div class="container">
+    <div>
+      <div id="wetPIcont" class="container">
         <div class="row">
-          <div class="col-lg-12">                  
+          <div class="col-lg-12">    
+            <Title>
+              WetPi
+            </Title>              
+            <div id="search"> 
+              <select id="selectCity" placeholder="pick a city..">
+              <option value="Pick a City">Pick a City</option>
+              </select><Find>Show</Find>
+            </div>
           </div>
         </div>
       </div>
