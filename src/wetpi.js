1dd1c15efbd904f79badc0b8d515f07dced57543
Daniel Rosel <danalves24@outlook.com>
2020-04-06 10:07:22 +0200

    customization

---
+++ src/wetpi.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+import $ from 'jquery';
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">                  
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
